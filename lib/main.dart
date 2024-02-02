9922df45b3e52e8d325eb72293c61a79c51ae4da
ambientProduct <156154385+ambientProduct@users.noreply.github.com>
2024-02-02 13:38:04 +0900

    Update main.dart
    Listenして更新書類のfield:nameを表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:provider/provider.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'backend/firebase/firebase_config.dart';
@@ -25,15 +27,6 @@ void main() async {
   ));
 }
 
-class MyApp extends StatefulWidget {
-  // This widget is the root of your application.
-  @override
-  State<MyApp> createState() => _MyAppState();
-
-  static _MyAppState of(BuildContext context) =>
-      context.findAncestorStateOfType<_MyAppState>()!;
-}
-
 class _MyAppState extends State<MyApp> {
   Locale? _locale;
   ThemeMode _themeMode = ThemeMode.system;
@@ -47,6 +40,22 @@ class _MyAppState extends State<MyApp> {
 
     _appStateNotifier = AppStateNotifier.instance;
     _router = createRouter(_appStateNotifier);
+
+    // Firestoreのデータを常時監視する
+    FirebaseFirestore.instance
+        .collection('ItemDetail')
+        .where('State', isEqualTo: 'Order')
+        .snapshots()
+        .listen((QuerySnapshot snapshot) {
+      for (final change in snapshot.docChanges) {
+        if (change.type == DocumentChangeType.added) {
+          // 新しく追加されたドキュメントの処理を行う
+          final document = change.doc;
+          final fieldValue = document.get('name');
+          print('New document added: $fieldValue');
+        }
+      }
+    });
   }
 
   void setLocale(String language) {
@@ -74,6 +83,7 @@ class _MyAppState extends State<MyApp> {
       ],
       theme: ThemeData(
         brightness: Brightness.light,
+        scrollbarTheme: ScrollbarThemeData(),
       ),
       themeMode: _themeMode,
       routerConfig: _router,
